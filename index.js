fc116b47483d18ce20e0ddb7ace7576dfbc6f11f
Zamin Ahmadi <asam79961@gmail.com>
2023-10-15 01:28:09 +1100

    finish the project

--- index.js
+++ index.js
@@ -1,29 +1,67 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const apiKey = '82192cd41da5bac0a2640aff81a620e0';
     // Search Icon
     const search = document.querySelector('.search-label');
 
     // Input value - 1. Get the input value from search 
     const searchInput = document.getElementById('search');
+    // city name field
+    const city_name = document.querySelector('.city-name');
+    const date = document.querySelector('.date');
+    const weekDay = date.querySelector('.week-day');
+    const weatherIcon = document.querySelector('.weather-status');
+    const temperature = document.querySelector('.temp');
+
+    // humidity field and wind speed field
+    const humidity = document.querySelector('.humidity-lvl');
+    const windSpeed = document.querySelector('.wind-speed');
+
+    // DEFAULT WEATHER DATA OF MELBOURNE START
+    
+    // call api for melbourne
+    const melbrnData = `http://api.openweathermap.org/data/2.5/weather?q=Melbourne&appid=${apiKey}`;
+    fetch(melbrnData)
+        .then(res => res.json())
+        .then(data => {
+            city_name.innerHTML = data.name;
+            
+            if (data.weather[0].main === 'Clear') {
+                weatherIcon.src = 'assets/sun.png';
+            } else if (data.weather[0].main === 'Rain') {
+                weatherIcon.src = 'assets/rain.png';
+            } else if (data.weather[0].main === 'Clouds') {
+                weatherIcon.src = 'assets/windy.png';
+            }
+
+            temperature.innerHTML = Math.floor(data.main.temp - 273.15) + '°C';
+            humidity.innerHTML = Math.floor(data.main.humidity) + '%';
+            windSpeed.innerHTML = data.wind.speed + 'm/s';
+        });
+    const melbrnDate = new Date();
+    const formatOps = {
+        weekday: 'short',
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true,
+    };
+    const formatted = melbrnDate.toLocaleString('en-US', formatOps);
+    date.innerHTML = formatted;
+
+    // DEFAULT VALUES FOR MELBOURNE WEATHER END 
+
+
 
     search.addEventListener('click', () => {
         searchInput.style.display = 'block';
     });
 
 
-
-    // 3. Get the input -> data from API ---> update the
-    //    'city-name' field
-    //    'date': to date.now -> formate:  DAY/TIME AM/PM
-    // 4. 'weather-status': update the weather status image based on weather status
-    // 5. Update the c.degree value of 'temp' field.
-    // 6. Create a div called 'weekly-data'
-    //    inside this div: 'day' 'status' 'day-temp'
-
-
     searchInput.addEventListener('keydown', (e) => {
         if(e.key == 'Enter' || e.key == 'Return') {
             e.preventDefault();
-            getWeatherData(searchInput);
+            getCurrentWeatherData(searchInput);
+            searchInput.style.display = 'none';
+            searchInput.value = '';
         }
     });
 
@@ -36,83 +74,61 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    async function getWeatherData(city) {
-        const apiKey = '82192cd41da5bac0a2640aff81a620e0';
+    async function getCurrentWeatherData(city) {
         const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}`);
         let data = await response.json();
         
         console.log(data);
-        // city name field
-        document.querySelector('.city-name').innerHTML = data.name;
+
+        city_name.innerHTML = data.name;
         
         // Getting the timezone of the city
-        const date = document.querySelector('.date');
-        const dateTime = getTimezone(data.coord.lon, data.coord.lat);
-        date.innerHTML = dateTime;    
+        getTimezone(data.coord.lat, data.coord.lon);
 
         // weather status field - img element
-        const weatherIcon = document.querySelector('.weather-status');
         if (data.weather[0].main === 'Clear') {
             weatherIcon.src = 'assets/sun.png';
         } else if (data.weather[0].main === 'Rain') {
             weatherIcon.src = 'assets/rain.png';
-        } else if (data.weather[0].main === 'Cloud') {
+        } else if (data.weather[0].main === 'Clouds') {
             weatherIcon.src = 'assets/windy.png';
         }
         
-
         // temperature field
-        const temperature = document.querySelector('.temp').innerHTML = Math.floor(data.main.temp - 273.15) + '°C';
-        
-        // weekdays-temp container
-        const weekdays = document.querySelector('.weekdays-temp');
-        const weeklyData = document.querySelector('.weekly-data');
-        const day = document.querySelector('.day');
-        const status = document.querySelector('.status');
-        const dayTemp = document.querySelector('.day-temp');
-
+        temperature.innerHTML = Math.floor(data.main.temp - 273.15) + '°C';
+        humidity.innerHTML = Math.floor(data.main.humidity) + '%';
+        windSpeed.innerHTML = data.wind.speed + 'm/s'; 
     }
 
-    async function getTimezone(location) {
-        const apiKey = 'AIzaSyDYu0TIOT9S06VT1GUTGiybvhkPHdgu7ns';
-        const timestamp = Math.floor(Date.now() / 1000);
-    
-        const apiUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${location}&timestamp=${timestamp}&key=${apiKey}`
-        const res = await fetch(apiUrl);
-        let data = await res.json();
-    
+    async function getTimezone(lat, lng) {
+        const apiKey = 'Y5LD8RBAD95O';
+        const url = `http://api.timezonedb.com/v2.1/get-time-zone?key=${apiKey}&format=json&by=position&lat=${lat}&lng=${lng}`;
         try {
-            const res = await fetch(apiUrl);
+            const res = await fetch(url);
             const data = await res.json();
-    
-            console.log(data);
-    
-            if(data.status === 'ok') {
-                const timeZoneId = data.timeZoneId;
-                const currentTime = new Date().toLocaleString('en-US', { timeZone: timeZoneId });
-        
+            
+            if(data.status === 'OK') {
+                console.log(data);
+                
                 // Format the time as 'MON 11AM'
+                const rawDate = new Date(data.formatted);
                 const options = {
                     weekday: 'short',
                     hour: 'numeric',
                     minute: '2-digit',
                     hour12: true,
                 };
-                const formattedTime = new Date(currentTime).toLocaleString('en-US', options);
-                return formattedTime;
-    
+                const newDate = rawDate.toLocaleDateString('en-US', options);
+                date.innerHTML = newDate;
+                console.log(newDate);
             } else {
                 throw new Error('Time zone data not available');
             }
         } catch(err) {
             throw new Error('Error fetching time zone data: ' + err.message);
         }
-        
-    
     }
-
-
-
+    
 });
 
 
